94c728bac8379b538ccb0623a7008e103c0f27f8
sapinspys <garri009@fiu.edu>
2019-11-14 20:06:47 -0800

    Added two more queries

--- module1-introduction-to-sql/rpg_queries.py
+++ module1-introduction-to-sql/rpg_queries.py
@@ -27,13 +27,18 @@ print("How many total Items?")
 c.execute('SELECT COUNT(*) FROM armory_item')
 print(c.fetchone(), '\n')
 
-
-
 print("How many of the Items are weapons? How many are not?")
-
+c.execute('SELECT COUNT(*) FROM armory_weapon')
+print(c.fetchone(), '\n')
 
 print("How many Items does each character have? (Return first 20 rows)")
+c.execute('SELECT COUNT(item_id), character_id FROM charactercreator_character_inventory GROUP BY character_id')
+print(c.fetchmany(20), '\n')
+
 print("How many Weapons does each character have? (Return first 20 rows)")
+c.execute('SELECT COUNT(item_id), character_id FROM charactercreator_character_inventory GROUP BY character_id')
+print(c.fetchmany(20), '\n')
+
 print("On average, how many Items does each Character have?")
 print("On average, how many Weapons does each character have?")
 
