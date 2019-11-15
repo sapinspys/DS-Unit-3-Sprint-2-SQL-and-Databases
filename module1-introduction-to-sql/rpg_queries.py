ed463ae2226efa49559c905e84e8a10996996e6a
sapinspys <garri009@fiu.edu>
2019-11-14 20:53:54 -0800

    All queries completed

--- module1-introduction-to-sql/rpg_queries.py
+++ module1-introduction-to-sql/rpg_queries.py
@@ -9,16 +9,16 @@ print(c.fetchone(), '\n')
 
 print("How many of each specific subclass?")
 c.execute("SELECT COUNT(*) FROM charactercreator_cleric")
-print('Clerics:', c.fetchone(), '\n')
+print('Clerics:', c.fetchone())
 
 c.execute("SELECT COUNT(*) FROM charactercreator_fighter")
-print('Fighters:', c.fetchone(), '\n')
+print('Fighters:', c.fetchone())
 
 c.execute("SELECT COUNT(*) FROM charactercreator_mage")
-print('Mages:', c.fetchone(), '\n')
+print('Mages:', c.fetchone())
 
 c.execute("SELECT COUNT(*) FROM charactercreator_necromancer")
-print('Necromancers:', c.fetchone(), '\n')
+print('Necromancers:', c.fetchone())
 
 c.execute("SELECT COUNT(*) FROM charactercreator_thief")
 print('Thiefs:', c.fetchone(), '\n')
@@ -32,15 +32,20 @@ c.execute('SELECT COUNT(*) FROM armory_weapon')
 print(c.fetchone(), '\n')
 
 print("How many Items does each character have? (Return first 20 rows)")
-c.execute('SELECT COUNT(item_id), character_id FROM charactercreator_character_inventory GROUP BY character_id')
+c.execute('SELECT COUNT(item_id) AS item_count, character_id FROM charactercreator_character_inventory GROUP BY character_id')
 print(c.fetchmany(20), '\n')
 
 print("How many Weapons does each character have? (Return first 20 rows)")
-c.execute('SELECT COUNT(item_id), character_id FROM charactercreator_character_inventory GROUP BY character_id')
+c.execute('SELECT COUNT(i.item_id) weapon_count, i.character_id FROM charactercreator_character_inventory i JOIN armory_weapon a ON i.item_id = a.item_ptr_id GROUP BY character_id')
 print(c.fetchmany(20), '\n')
 
 print("On average, how many Items does each Character have?")
+c.execute('SELECT AVG(item_id) AS item_count_avg, character_id FROM charactercreator_character_inventory GROUP BY character_id')
+print(c.fetchmany(20), '\n')
+
 print("On average, how many Weapons does each character have?")
+c.execute('SELECT AVG(i.item_id) weapon_count_avg, i.character_id FROM charactercreator_character_inventory i JOIN armory_weapon a ON i.item_id = a.item_ptr_id GROUP BY character_id')
+print(c.fetchmany(20), '\n')
 
 # USE TO SAVE WORK IF SOMETHING CHANGEd
 # SQLITE3 AUTO-SAVES THOUGH
