4d032bcc28c5a5117ec13e9c0a84f33eff60830f
sapinspys <garri009@fiu.edu>
2019-11-22 18:38:12 -0800

    Completed part 1, demo_data script

--- Sprint Challenge/demo_data.py
+++ Sprint Challenge/demo_data.py
@@ -3,8 +3,27 @@ import sqlite3
 conn = sqlite3.connect('demo_data.sqlite3')
 c = conn.cursor()
 
-# USE TO SAVE WORK IF SOMETHING CHANGEd
+# Create table
+c.execute("CREATE TABLE IF NOT EXISTS demo (S varchar(255), X int, Y int);")
+
+# Truncate demo table
+c.execute("DELETE FROM demo")
+c.execute("INSERT INTO demo (S, X, Y) values ('g', '3', '9');")
+c.execute("INSERT INTO demo (S, X, Y) values ('v', '5', '7');")
+c.execute("INSERT INTO demo (S, X, Y) values ('f', '8', '7');")
+
+# USE TO SAVE WORK IF SOMETHING CHANGED
 # SQLITE3 AUTO-SAVES THOUGH
-# conn.commit() 
+conn.commit() 
+
+# Test with queries
+c.execute("SELECT COUNT(*) FROM demo")
+print(f'Total rows: {c.fetchone()}')
+
+c.execute("SELECT COUNT(*) FROM demo WHERE X>=5 AND Y>=5")
+print(f'Total number of rows where x and y are at least 5: {c.fetchone()}')
+
+c.execute("SELECT COUNT(DISTINCT Y) FROM demo")
+print(f'Total unique values of y: {c.fetchall()}')
 
 conn.close()
